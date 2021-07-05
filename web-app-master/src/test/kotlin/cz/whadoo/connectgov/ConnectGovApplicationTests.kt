763b206e35e3137d10e16be5a5a7e889b292a609
Marek Szeles <marek.szeles@gmail.com>
2021-07-05 17:55:13 +0200

    refactor

--- web-app-master/src/test/kotlin/cz/whadoo/connectgov/ConnectGovApplicationTests.kt
+++ web-app-master/src/test/kotlin/cz/whadoo/connectgov/ConnectGovApplicationTests.kt
@@ -1,4 +1,4 @@
-package cz.whadoo.smaprt
+package cz.whadoo.connectgov
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class SmaprtApplicationTests {
+class ConnectGovApplicationTests {
 
 	@Test
 	fun contextLoads() {
